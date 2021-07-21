71e18e5f5608a03a78fb3815300f74484dcc3067
juz <electronicallyreached@gmail.com>
2021-07-21 17:42:27 -0500

    Create Jumbotron

---
+++ client/src/components/Jumbotron.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Jumbotron() {
+  return (
+    <div className='p-4 mb-4 bg-gray-700'>
+      <h1 className='text-center text-3xl font-semibold tracking-wide text-gray-200'> WeatherDash </h1>
+    </div>
+  )
+}
