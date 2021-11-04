669ce0a06b806d76e0575f26521cdcaf65330c13
juz <electronicallyreached@gmail.com>
2021-11-04 14:01:35 -0500

    Updates

--- client/src/components/NotFound.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export default function NotFound({ code, message }) {
-  return (
-    <p className='text-gray-300 text-3xl font-thin ring-gray-600 ring-4 rounded-xl p-12 '> {code}: {message} </p>
-  )
-}
